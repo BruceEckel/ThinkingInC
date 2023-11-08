d9793d6c1ada0d74022be39163bd87d2809c747a
Bruce Eckel <MindviewInc@gmail.com>
2023-11-08 14:14:38 -0800

    Ran rye sync

--- tools/make_slides/make_slides.py
+++ tools/make_slides/make_slides.py
@@ -1,73 +1,90 @@
+# generate.py
+# Generates "slides.html" for each lecture directory in "Thinking in C"
 from pathlib import Path
-
-header = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thinking in C</title>
-    <style>
-        body { font-family: Arial, sans-serif; margin: 0; display: flex; flex-direction: column; height: 100vh; }
-        img { max-width: 100%; max-height: 80vh; margin: auto; }
-        footer {
-            margin-top: auto;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 1em;
-        }
-        audio { 
-            flex-grow: 0;   /* Ensure it doesn't grow beyond its width */
-            width: 600px;   /* Fixed width for the audio control */
-            margin-left: 1em;
-        }
-    </style>
-</head>
-<body>
-"""
-
-footer = """
-</body>
-</html>
-"""
+import json
 
 
-def generate_html_for_lecture(lecture_directory):
-    # Get all SVG and MP3 files
+def generate_html_with_javascript(lecture_directory: Path) -> None:
     svg_files = sorted(lecture_directory.glob("*.svg"))
     mp3_files = sorted(lecture_directory.glob("*.mp3"))
+    slides = [
+        {"svg": svg.name, "mp3": svg.with_suffix(".mp3").name}
+        for svg in svg_files
+        if svg.with_suffix(".mp3") in mp3_files
+    ]
+    slides_json = json.dumps(slides)
+    html_content = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Thinking in C</title>
+        <style>
+            /* Add your CSS styles here */
+        </style>
+    </head>
+    <body>
+        <img id="slideImage" alt="Slide" style="max-width: 100%; max-height: 80vh; margin: auto;">
+        <br/>
+        <button id="previousButton" onclick="previousSlide()">Previous</button>
+        <button id="nextButton" onclick="nextSlide()">Next</button>
+        <audio id="slideAudio" controls style="width: 600px; margin-left: 1em;"></audio>
+        <button onclick="goHome()">Home</button>
+        <script>
+            const slides = {slides_json};
+            let currentSlide = 0;
 
-    for idx, svg in enumerate(svg_files):
-        mp3 = svg.with_suffix(".mp3")
-        if mp3 not in mp3_files:
-            print(f"No associated MP3 found for {svg.name}. Skipping...")
-            continue
+            function showSlide(index) {{
+                const slide = slides[index];
+                document.getElementById('slideImage').src = slide.svg;
+                document.getElementById('slideAudio').src = slide.mp3;
+                currentSlide = index;
+                document.getElementById('slideAudio').play();
+                updateNavigationButtons();
+            }}
 
-        with open(lecture_directory / f"slide_{idx + 1}.html", "w") as f:
-            f.write(header)
+            function nextSlide() {{
+                if (currentSlide < slides.length - 1) {{
+                    showSlide(currentSlide + 1);
+                }}
+            }}
 
-            # SVG and MP3 content
-            f.write(f'<img src="{svg.name}" alt="Slide {idx + 1}">')
+            function previousSlide() {{
+                if (currentSlide > 0) {{
+                    showSlide(currentSlide - 1);
+                }}
+            }}
 
-            # Navigation and audio at the bottom
-            f.write("<footer>")
-            if idx != 0:
-                f.write(
-                    f'<a href="slide_{idx}.html">Previous</a>&nbsp;&nbsp;&nbsp;&nbsp;'
-                )
-            if idx != len(svg_files) - 1:
-                f.write(f'<a href="slide_{idx + 2}.html">Next</a>')
-            f.write(
-                f'<audio controls autoplay><source src="{mp3.name}" type="audio/mpeg"></audio>'
-            )
-            f.write('&nbsp;&nbsp;&nbsp;<a href="../../Index.html">Home</a>')
-            f.write("</footer>")
+            function updateNavigationButtons() {{
+                document.getElementById('previousButton').style.display = currentSlide === 0 ? 'none' : 'inline';
+                document.getElementById('nextButton').style.display = currentSlide === slides.length - 1 ? 'none' : 'inline';
+            }}
 
-            f.write(footer)
+            function goHome() {{
+                window.location.href = '../../Index.html';
+            }}
+
+            document.getElementById('slideAudio').addEventListener('ended', nextSlide);
+
+            window.onload = () => {{
+                showSlide(0);
+                updateNavigationButtons();
+            }};
+        </script>
+    </body>
+    </html>
+    """
+
+    with open(lecture_directory / "slides.html", "w") as f:
+        f.write(html_content)
+
+    print(
+        f"Single HTML file with JavaScript created successfully for {lecture_directory.name}!"
+    )
 
 
-def generate_html(directory="."):
+def generate_single_html(directory: str = ".") -> None:
     main_directory = Path(directory)
     lectures_directory = main_directory / "lectures"
 
@@ -77,11 +94,11 @@ def generate_html(directory="."):
 
     for lecture_dir in lectures_directory.iterdir():
         if lecture_dir.is_dir():
-            print(f"Generating slides for lecture: {lecture_dir.name}")
-            generate_html_for_lecture(lecture_dir)
+            print(f"Generating single HTML for lecture: {lecture_dir.name}")
+            generate_html_with_javascript(lecture_dir)
 
-    print("HTML files generated successfully!")
+    print("All lectures have been processed.")
 
 
 if __name__ == "__main__":
-    generate_html(Path("../../src"))
+    generate_single_html(Path("../../src"))
